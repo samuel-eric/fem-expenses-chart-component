b395cc0126bdbdbc907d35155d54d123b3a34818
samuel-eric <samueltan086@gmail.com>
2024-01-11 09:25:19 +0700

    setting up tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'soft-red': 'hsl(10, 79%, 65%)',
+				cyan: 'hsl(186, 34%, 60%)',
+				'dark-brown': 'hsl(25, 47%, 15%)',
+				'medium-brown': 'hsl(28, 10%, 53%)',
+				cream: 'hsl(27, 66%, 92%)',
+				'pale-orange': 'hsl(33, 100%, 98%)',
+			},
+		},
+		fontFamily: {
+			body: ['"DM Sans"'],
+		},
 	},
 	plugins: [],
 }
